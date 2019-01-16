903654c1f33aeef02b5114c63935f7b328541af3
Babs <bblanfort@gmail.com>
2019-01-16 15:45:36 -0800

    Add and connect to db using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
-const express = require("express"); // did double quotes change after saving?
-const app = express();
+const express = require('express'); // did double quotes change after saving?
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './variables.env' });
 
 const PORT = process.env.PORT || 4444;
 
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
+
+const app = express();
+
 app.listen(PORT, () => {
-  console.log(`Your Server listening on PORT ${PORT}`);
+  console.log(`Your Server is listening on PORT ${PORT}`);
 });
