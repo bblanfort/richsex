51da72fe83469b4f9db9531a3cac835c0c5c976f
Babs <bblanfort@gmail.com>
2019-01-30 15:02:39 -0800

    learn basics of react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h1 className="title">I am awesome</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
